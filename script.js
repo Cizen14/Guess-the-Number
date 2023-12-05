8e95519274a9b8eb822a094e3daaf51c807fdf19
Cizen <cizenmah@gmail.com>
2023-12-05 12:27:24 +1100

    fixed a bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Generating the Random Number
-let x = Math.floor(Math.random() * 20 + 1);
+let y = Math.floor(Math.random() * 20 + 1);
 
 //Coutning the Number of guesses
 //made for correct Guess
@@ -8,17 +8,17 @@ let guess = 1;
 document.getElementById("submitguess").onclick = function (){
     //Number guessed by User
 
-    let y = document.getElementById("guessField").value;
+    let x = document.getElementById("guessField").value;
     if (x == y){
         alert("CONGRATULATIONS !!! YOU GUESSED IT RIGHT" +   guess   +  "GUESS");
 
     }
-    else if (x < y){
+    else if (y < x){
         guess++;
         alert("OPPS SORRY!! THE ONE YOU SELECTED IT WRONG. TRY A SMALLER NUMBER");
     }
    
-    else (x > y)
+    else 
     {
         guess++;
         alert("OPPS SORRY!! THE ONE YOU SELECTED IT WRONG. TRY A LARGER NUMBER")
