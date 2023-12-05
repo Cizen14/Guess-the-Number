3d796c7a9bbf2478f9555909e8272220ca6d3d88
Cizen <cizenmah@gmail.com>
2023-12-05 12:17:03 +1100

    Javascript added

--- script.js
+++ script.js
@@ -1,10 +1,29 @@
 // Generating the Random Number
-let x = (Math.random()*20+1);
+let x = Math.floor(Math.random() * 20 + 1);
 
 //Coutning the Number of guesses
 //made for correct Guess
 
 let guess = 1;
-document
+document.getElementById("submitguess").onclick = function (){
+    //Number guessed by User
+
+    let y = document.getElementById("guessField").value;
+    if (x == y){
+        alert("CONGRATULATIONS !!! YOU GUESSED IT RIGHT" +   guess   +  "GUESS");
+
+    }
+    else if (x < y){
+        guess++;
+        alert("OPPS SORRY!! THE ONE YOU SELECTED IT WRONG. TRY A SMALLER NUMBER");
+    }
+   
+    else (x > y)
+    {
+        guess++;
+        alert("OPPS SORRY!! THE ONE YOU SELECTED IT WRONG. TRY A LARGER NUMBER")
+    }
+
+}
 
 
