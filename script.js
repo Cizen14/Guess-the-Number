a7df1d0b20cf3de82a55e6f9e0506dca9798dcf6
Cizen <cizenmah@gmail.com>
2023-12-11 11:31:12 +1100

    added the error alert for selecting the invalid number

--- script.js
+++ script.js
@@ -9,21 +9,31 @@ document.getElementById("submitguess").onclick = function (){
     //Number guessed by User
 
     let x = document.getElementById("guessField").value;
+    
+  
     if (x == y){
         alert("CONGRATULATIONS !!! YOU GUESSED IT RIGHT " + guess  +  " GUESS");
 
+    }
+    else if (x>=20){
+        alert("Error!! The number is between 0-20");
     }
     else if (y < x){
         guess++;
         alert("OPPS SORRY!! THE ONE YOU SELECTED IT WRONG. TRY A SMALLER NUMBER");
     }
+    else if (x<0){
+        alert("Error!! The number is between 0-20")
+    }
+   
    
     else 
     {
         guess++;
         alert("OPPS SORRY!! THE ONE YOU SELECTED IT WRONG. TRY A LARGER NUMBER")
     }
-
 }
+
+
 
 
